76f9f8e51bc696627d53b6f75132e1b2e7884eaa
Chris Webb <chris@arachsys.com>
2024-08-14 14:10:57 +0100

    Use static inline for dummy compat functions

--- openbsd/compat.h
+++ openbsd/compat.h
@@ -1,5 +1,11 @@
 #define __dead __attribute__((__noreturn__))
 #define __packed __attribute__((__packed__))
 
-int setsync(int synced);
-void initproctitle(char **argv, char **envp);
+static inline int setsync(int synced)
+{
+	return 0;
+}
+
+static inline void initproctitle(char **argv, char **envp)
+{
+}
